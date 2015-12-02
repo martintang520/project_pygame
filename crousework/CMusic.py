151069f5dd2c98bbcfd8f4a11cc5531487b61d72
jerryjiao <jerry201382@gmail.com>
2015-12-02 11:55:42 +0000

    Cmusic comment

--- crousework/CMusic.py
+++ crousework/CMusic.py
@@ -2,65 +2,67 @@ import pygame
 
 
 class sound:
-       def __init__(self,a):
+       def __init__(self,strmusicname):
              
-              self.a=a
+              self.strmusicname=strmusicname
               pygame.mixer.init()
               self.State = True
-              b="sound/"+self.a
+              strsoundname="sound/"+self.strmusicname #the path of music file#
               channel=pygame.mixer.find_channel()
-              self.sound = pygame.mixer.Sound(b)
-       #0 loop ,n means times#
-       def play(self,c):
-              if c ==0:
+              self.sound = pygame.mixer.Sound(strsoundname)
+       #function play sound,0 means loop play,n means n times#
+       def play(self,ntimes):
+              if ntimes ==0:
                      self.sound.play(-1,0)
               else:
-                     self.sound.play(c-1,0)
+                     self.sound.play(ntimes-1,0)
+       #function stop music#
        def stop(self):             
               self.sound.stop()
-       #c between 0-1#
-       def setvolumn(self,c):
-              self.c=c
-              v=c/100.0
-              self.sound.set_volume(v)
-       #get sound state true or false
+       #function set play volumn,c between 0-1#
+       def setvolumn(self,nvolumn):
+              self.nvolumn=nvolumn
+              nvolumn=nvolumn/100.0
+              self.sound.set_volume(nvolumn)
+       #get sound state true or false#
        def soundState(self):
               return self.State
 
 
 class music:
-       def __init__(self,b=""):
-              b="music/"+b
+       def __init__(self,strmusicname):
+              strmusicname="music/"+strmusicname
               pygame.mixer.init()
-              track = pygame.mixer.music.load(b)
+              track = pygame.mixer.music.load(strmusicname)
               pygame.mixer.music.set_volume(0.5)
               self.State = False
               self.c=100
-       def play(self,a):
-              if a == 0:
+       #function play music#
+       def play(self,ntimes):
+              if ntimes == 0:
                      pygame.mixer.music.play(-1,0.0)
               else:
-                     pygame.mixer.music.play(c,0.0)
+                     pygame.mixer.music.play(ntimes,0.0)
               self.State = True
-              
+       #function music Pause#
        def pause(self):
               pygame.mixer.music.pause()
               self.State = False
-              
+       #function music stop#
        def stop(self):
               pygame.mixer.music.stop()
               self.State = False
-              
-       def setvolumn(self,c):
-              self.c=c
-              v=c/100.0
-              pygame.mixer.music.set_volume(v)
-
+       #function set music volumn, c beween 0-1#      
+       def setvolumn(self,nvolumn):
+              self.nvolumn=nvolumn
+              nvolumn=nvolumn/100.0
+              pygame.mixer.music.set_volume(nvolumn)
+       #return music state#
        def musicState(self):
               return self.State
-
+       #return music volumn#
        def musicVolumn(self):
-              return self.c
+              return self.nvolumn
 
 
 
