887c94c51237b375f102c4d414812f64a2e8b302
CastleDeng <wsdxyhh@163.com>
2015-12-02 11:19:18 +0000

    Tower comment

--- crousework/CTower.py
+++ crousework/CTower.py
@@ -1,30 +1,29 @@
 import pygame
-
 from pygame.locals import*
-
 from CButton import *
 
-
-
-
-
 class CTower(CButton):
+    
 #Tower name dictionary
-    dictTowerName={-1:"picture/cross.png",0:"picture/blank.png",1:"picture/tower1.png",2:"picture/tower2.png",4:"picture/tower4.png",5:"picture/tower5.png"}
+    dictTowerName={-1:"picture/cross.png",0:"picture/blank.png",
+                   1:"picture/tower1.png",2:"picture/tower2.png",
+                   4:"picture/tower4.png",5:"picture/tower5.png"}
+
 #Tower Attack Point
     dictTowerAttackPoint = {1:10,2:5,4:20,5:10}
 
-    def __init__(self, color, initialPos, size,Towertype):
+    def __init__(self, color, initialPos, size, Towertype):
 
         CObject.__init__(self, color, initialPos, size, self.dictTowerName[Towertype])
 
-        self.nTowerType=Towertype
+        self.nTowerType = Towertype
         self.attacktimer = 2
         self.GameInit()
 
     def GameInit(self):
         self.rangeTower = 192 #tower attack range 192 are 3 block
-        if self.nTowerType>0:
+        #set attack point
+        if self.nTowerType > 0:
             self.nAttackPoint = self.dictTowerAttackPoint[self.nTowerType]
 
     def Update(self, deltaTime):
@@ -34,17 +33,20 @@ class CTower(CButton):
 
     def Render(self, deltaTime, surface):
         CObject.Render(self, deltaTime, surface)
+        
 #position tulple like (300,100)
     def GetPosition(self):
         return self.tulPos
+    
 #Size tulple like (64,64)
     def GetSize(self):
         return self.tulSize
+    
 # set tower type. like type 1 type 2 type 3 type 4
-    def SetType(self,Towertype):
-        self.nTowerType=Towertype
+    def SetType(self, Towertype):
+        self.nTowerType = Towertype
         self.SetImage(self.dictTowerName[Towertype])
-        if self.nTowerType>0:
+        if self.nTowerType > 0:
             self.nAttackPoint = self.dictTowerAttackPoint[self.nTowerType]
 
 
